2483289f0e082c8254c2942b24d285c23c3b47d8
houhan@gmail.com <han.hou@alleninstitute.org>
2025-03-06 03:49:09 +0000

    renaming more fields

--- src/LCNE_patchseq_analysis/data_util/metadata.py
+++ src/LCNE_patchseq_analysis/data_util/metadata.py
@@ -25,22 +25,32 @@ def read_brian_spreadsheet(file_path=file_path):
     # Get xyz coordinates
     tab_xyz = [name for name in tab_names if "xyz" in name.lower()][0]
     df_xyz = pd.read_excel(file_path, sheet_name=tab_xyz)
-    df_xyz.rename(
-        columns={
-            "specimen_name": "jem-id_cell_specimen",
-            "structure_acronym": "Annotated structure",
-        },
-        inplace=True,
-    )
 
     # Get ephys features
     tab_ephys_fx = [name for name in tab_names if "ephys_fx" in name.lower()][0]
     df_ephys_fx = pd.read_excel(file_path, sheet_name=tab_ephys_fx)
 
     # Merge the tables
-    df_all = df_master.merge(df_xyz, on="jem-id_cell_specimen", how="outer", suffixes=('_master', '_xyz')).merge(
-        df_ephys_fx, on="cell_specimen_id", how="outer", suffixes=('_master', 'ephys_fx')
-    ).sort_values("Date", ascending=False)
+    df_all = (
+        df_master.merge(
+            df_xyz.rename(
+                columns={
+                    "specimen_name": "jem-id_cell_specimen",
+                    "structure_acronym": "Annotated structure",
+                }
+            ),
+            on="jem-id_cell_specimen",
+            how="outer",
+            suffixes=("_master", "_xyz"),
+        )
+        .merge(df_ephys_fx.rename(
+                columns={
+                    "failed_seal": "failed_no_seal",
+                    "failed_input_access_resistance": "failed_bad_rs",
+                }
+            ), on="cell_specimen_id", how="outer", suffixes=("_master", "_ephys_fx"))
+        .sort_values("Date", ascending=False)
+    )
 
     return df_all, df_master, df_xyz, df_ephys_fx
 
