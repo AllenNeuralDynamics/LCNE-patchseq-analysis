ad4386bd06e80d427c489d9dc3982d4e4f3e5e79
Han Hou <houhan@gmail.com>
2025-03-06 11:48:53 -0800

    linting

--- src/LCNE_patchseq_analysis/data_util/lims.py
+++ src/LCNE_patchseq_analysis/data_util/lims.py
@@ -3,22 +3,27 @@
 From Brian
 """
 
-import pg8000          #pg8000 access SQL databases
-import pandas as pd    #pandas will be needed to work in a dataframe
+import pg8000  # pg8000 access SQL databases
+import pandas as pd  # pandas will be needed to work in a dataframe
 
-#code from Agata
-#these are nice functions to open LIMS, make a query and then close LIMS after
+# code from Agata
+# these are nice functions to open LIMS, make a query and then close LIMS after
+
 
 def _connect(user="limsreader", host="limsdb2", database="lims2", password="limsro", port=5432):
     conn = pg8000.connect(user=user, host=host, database=database, password=password, port=port)
     return conn, conn.cursor()
 
+
 def _select(cursor, query):
     cursor.execute(query)
-    columns = [ d[0] for d in cursor.description ]
-    return [ dict(zip(columns, c)) for c in cursor.fetchall() ]
+    columns = [d[0] for d in cursor.description]
+    return [dict(zip(columns, c)) for c in cursor.fetchall()]
 
-def limsquery(query, user="limsreader", host="limsdb2", database="lims2", password="limsro", port=5432):
+
+def limsquery(
+    query, user="limsreader", host="limsdb2", database="lims2", password="limsro", port=5432
+):
     """A function that takes a string containing a SQL query, connects to the LIMS database and outputs the result."""
     conn, cursor = _connect(user, host, database, password, port)
     try:
@@ -29,14 +34,14 @@ def limsquery(query, user="limsreader", host="limsdb2", database="lims2", passwo
     return results
 
 
-#this last function will take our query results and put them in a dataframe so that they are easy to work with
+# this last function will take our query results and put them in a dataframe so that they are easy to work with
 def get_lims_dataframe(query):
-    '''Return a dataframe with lims query'''
+    """Return a dataframe with lims query"""
     result = limsquery(query)
     try:
         data_df = pd.DataFrame(data=result, columns=result[0].keys())
     except IndexError:
-        print ("Could not find results for your query.")
+        print("Could not find results for your query.")
         data_df = pd.DataFrame()
     return data_df
 
@@ -59,6 +64,7 @@ def get_lims_LCNE_patchseq():
     """
     lims_df = get_lims_dataframe(lims_query)
     return lims_df
+
 
 if __name__ == "__main__":
     lims_df = get_lims_LCNE_patchseq()
