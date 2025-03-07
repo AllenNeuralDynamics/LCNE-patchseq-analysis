121319126df0caa75ff87e794853efd79f1ca1c5
Han Hou <houhan@gmail.com>
2025-03-07 12:54:28 -0800

    renaming

--- src/LCNE_patchseq_analysis/data_util/ephys.py
+++ src/LCNE_patchseq_analysis/data_util/ephys.py
@@ -105,14 +105,14 @@ def upload_raw_from_isilon_to_s3_batch(df, s3_bucket=s3_bucket, max_workers=10):
 def trigger_patchseq_upload(metadata_path=os.path.expanduser(R"~\Downloads\IVSCC_LC_summary.xlsx")):
     # Generate a list of isilon paths
     dfs = read_brian_spreadsheet(file_path=metadata_path, add_lims=True)
-    df_merged = dfs["df_all"]
+    df_merged = dfs["df_merged"]
 
     # Upload raw data
     upload_raw_from_isilon_to_s3_batch(df_merged, s3_bucket=s3_bucket, max_workers=10)
 
     # Also save df_merged as csv and upload to s3
-    df_merged.to_csv("df_merged.csv", index=False)
-    sync_directory("df_merged.csv", s3_bucket + "/df_merged.csv", if_copy=True)
+    df_merged.to_csv("df_metadata_merged.csv", index=False)
+    sync_directory("df_metadata_merged.csv", s3_bucket + "/df_metadata_merged.csv", if_copy=True)
 
 
 if __name__ == "__main__":
