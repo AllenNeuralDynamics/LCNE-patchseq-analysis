a2bd27bf05f892f2910d55f12ed17c96872ff018
houhan@gmail.com <han.hou@alleninstitute.org>
2025-03-13 00:48:46 +0000

    minor

--- src/LCNE_patchseq_analysis/panel_nwb_viz.py
+++ src/LCNE_patchseq_analysis/panel_nwb_viz.py
@@ -40,11 +40,15 @@ def update_plot(n, hdf):
     stimulus = np.array(hdf[f"/stimulus/presentation/data_{n:05}_DA0/data"])
     time = np.arange(len(trace)) * dt_ms
     
-    fig, ax = plt.subplots(2, 1, figsize=(6, 4))
+    fig, ax = plt.subplots(2, 1, figsize=(6, 4), gridspec_kw={'height_ratios': [3, 1]})
     ax[0].plot(time, trace)
     ax[0].set_title(f'Sweep number {n}')
+    ax[0].set(ylabel='Vm (mV)')
     
     ax[1].plot(time, stimulus)
+    ax[1].set(xlabel='Time (ms)', ylabel='I (pA)')
+    ax[0].label_outer()
+    
     plt.close(fig)  # Prevents duplicate display
     return fig
   
@@ -56,14 +60,14 @@ def main():
     ephys_roi_id = "1410790193"
     raw_path_this = f"{RAW_DIRECTORY}/Ephys_Roi_Result_{ephys_roi_id}"
     
-    nwbs = glob.glob(f"{raw_path_this}/*.nwb")
+    nwbs = glob.glob(f"{raw_path_this}/*spikes.nwb")
     
     # Load NWB data (adjust the file path as needed)
-    hdf = load_h5py_data(nwbs[1])
+    hdf = load_h5py_data(nwbs[0])
     
     # Define a slider widget. Adjust the range based on your NWB data dimensions.
     n_sweeps = len(hdf["acquisition"])
-    slider = pn.widgets.IntSlider(name='Data Slice', start=0, end=n_sweeps-1, value=0)
+    slider = pn.widgets.IntSlider(name='Sweep number', start=0, end=n_sweeps-1, value=0)
     
     # Bind the slider value to the update_plot function.
     # pn.bind creates a reactive function that updates the plot whenever the slider changes.
