5c99a960db7ea2120ce46ba199473cac5f494d53
Hou, Han <houhan@gmail.com>
2025-04-01 12:45:24 -0700

    feat: set global eFEL settings in initialization

--- src/LCNE_patchseq_analysis/efel/__init__.py
+++ src/LCNE_patchseq_analysis/efel/__init__.py
@@ -1,6 +1,12 @@
 """Extracting features using eFEL."""
 
+import efel
+
 EFEL_SETTINGS = {
     "interp_step": 0.02,
     "Threshold": 0.0,
 }
+
+# Set global eFEL settings
+for setting, value in EFEL_SETTINGS.items():
+    efel.api.set_setting(setting, value)
