52bf370a0133fe46b8115359e8eac10f6bce2957
Hou, Han <houhan@gmail.com>
2025-04-01 15:04:22 -0700

    feat: implement trace packaging for eFEL analysis and logging

--- src/LCNE_patchseq_analysis/efel/single_cell.py
+++ src/LCNE_patchseq_analysis/efel/single_cell.py
@@ -1,58 +1,69 @@
 """Extracting features from a single cell."""
 
+import logging
+import multiprocessing as mp
+
 import efel
 import numpy as np
 import pandas as pd
 
-from LCNE_patchseq_analysis.efel import EFEL_SETTINGS
 from LCNE_patchseq_analysis.data_util.nwb import PatchSeqNWB
 
-
-def efel_single_sweep(ephys_roi_id, sweep_number):
-
+logger = logging.getLogger(__name__)
     
-    # Get trace and time for sweep 0 
-    trace = raw.get_raw_trace(sweep_number)
-    time = raw.dt_ms * np.arange(len(trace))
-    stimulus = raw.get_stimulus(sweep_number)
-    
-    meta_this = raw.df_sweeps.query("sweep_number == @sweep_number").copy()
-    meta_this.loc[:, "ephys_roi_id"] = ephys_roi_id
-    stim_code = meta_this["stimulus_code"].values[0]
-    stim_start = meta_this["stimulus_start_time"].values[0] * 1000
-    stim_end = stim_start + meta_this["stimulus_duration"].values[0] * 1000
 
-    # Use efel to get features
-    traces = [
-        {
+def pack_traces_for_efel(raw):
+    """Package traces for eFEL."""
+    # Only valid sweeps (passed not NA, and exclude CHIRP sweeps)
+    df_sweeps = raw.df_sweeps
+    valid_sweep_numbers = df_sweeps.loc[
+        (df_sweeps["passed"].notna()) &
+        ~(df_sweeps["stimulus_code"].str.contains("CHIRP", case=False)),
+        "sweep_number"
+    ].values
+
+    traces = []
+    for sweep_number in valid_sweep_numbers:
+        trace = raw.get_raw_trace(sweep_number)
+        time = raw.get_time(sweep_number)
+
+        meta_this = df_sweeps.query("sweep_number == @sweep_number")
+        stim_start = meta_this["stimulus_start_time"].values[0]
+        stim_end = stim_start + meta_this["stimulus_duration"].values[0]
+
+        # Use efel to get features
+        traces.append({
             "T": time,
             "V": trace,
             "stim_start": [stim_start],
             "stim_end": [stim_end],
-        }
-    ]
-    df_features = pd.DataFrame(efel.get_feature_values(
-        traces, efel.get_feature_names(), raise_warnings=False))
-    
-    return {
-        "stim_code": stim_code,
-        "time": time,
-        "trace": trace,
-        "meta": meta_this,
-        "stimulus": stimulus,
-        "stim_start": stim_start,
-        "stim_end": stim_end}, df_features
-    
+        })
+
+    logger.info(f"Packed {len(traces)} traces for eFEL.")
+    return traces, valid_sweep_numbers
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
     ephys_roi_id = "1212557784"
-    sweep_number = 9
     
+    # Get raw data
     raw = PatchSeqNWB(ephys_roi_id=ephys_roi_id)
-    df_sweeps = raw.df_sweeps
     
-    # Look for valid sweeps
-    valid_sweeps = df_sweeps.loc[df_sweeps["passed"].notna(), "sweep_number"]
+    # Package all valid sweeps for eFEL
+    traces, valid_sweep_numbers = pack_traces_for_efel(raw)
     
-    raw, df_features = efel_single_sweep(ephys_roi_id, sweep_number)
-    print(df_features)
+    # Get all features
+    logger.info(f"Getting features for {len(traces)} traces...")
+    features = efel.get_feature_values(
+        traces,
+        efel.get_feature_names(),  # Get all features
+        raise_warnings=False,
+    )
+    logger.info("Done!")
+    # Save features
+    df_features = pd.DataFrame(features, index=valid_sweep_numbers)
+    
+
+    
+    
