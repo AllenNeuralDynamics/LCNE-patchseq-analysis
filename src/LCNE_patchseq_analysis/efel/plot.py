1f41158f43d56b689fc61adf6cb8c7ed873fe4d7
Hou, Han <houhan@gmail.com>
2025-04-02 10:14:22 -0700

    refactor

--- src/LCNE_patchseq_analysis/efel/plot.py
+++ src/LCNE_patchseq_analysis/efel/plot.py
@@ -1,8 +1,12 @@
+"""Plotting functions for electrophysiology data."""
+
 import os
-from matplotlib.gridspec import GridSpec
+from typing import Dict, Any, List, Tuple
+
 import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
+import pandas as pd
+import seaborn as sns
 
 from LCNE_patchseq_analysis import TIME_STEP
 
@@ -10,39 +14,60 @@ sns.set_style("white")
 sns.set_context("talk")
 
 
-def plot_sweep_raw(raw_trace, df_sweep_meta, df_sweep_feature, df_spike_feature):
+def plot_sweep_raw(
+    raw_trace: Dict[str, Any],
+    df_sweep_meta: pd.DataFrame,
+    df_sweep_feature: pd.Series,
+    df_spike_feature: pd.DataFrame
+) -> plt.Figure:
+    """Plot raw sweep data with features.
+    
+    Args:
+        raw_trace: Dictionary containing raw trace data
+        df_sweep_meta: DataFrame containing sweep metadata
+        df_sweep_feature: Series containing sweep features
+        df_spike_feature: DataFrame containing spike features
+        
+    Returns:
+        Matplotlib figure object
+    """
+    time, trace, stimulus, stim_start, stim_end = (
+        raw_trace["T"],
+        raw_trace["V"],
+        raw_trace["stimulus"],
+        raw_trace["stim_start"][0],
+        raw_trace["stim_end"][0]
+    )
 
-    (time, trace, stimulus, stim_start, stim_end) = (
-        raw_trace["T"], raw_trace["V"], raw_trace["stimulus"], raw_trace["stim_start"][0], raw_trace["stim_end"][0])
-
-    time_interpolated = time  # This only works for non-interpolated case. (TIME_STEP = interp_step)
+    time_interpolated = time  # This only works for non-interpolated case
 
     # Plot the trace
     fig = plt.figure(figsize=(12, 6))
-    gs = GridSpec(2, 1, height_ratios=[5, 1])
+    gs = plt.GridSpec(2, 1, height_ratios=[5, 1])
     ax = fig.add_subplot(gs[0, 0])
     ax_stimulus = fig.add_subplot(gs[1, 0], sharex=ax)
 
     ax.plot(time, trace, 'k-', lw=2)
-    ax.axhline(df_sweep_feature["voltage_base"], color="k", linestyle="--", label="voltage_base")
+    ax.axhline(df_sweep_feature["voltage_base"], color="k", linestyle="--",
+               label="voltage_base")
 
     if df_sweep_feature["spike_count"] > 0:
         ax.plot(df_spike_feature["AP_begin_time"],
                 df_spike_feature["AP_begin_voltage"], "go",
                 label="AP_begin")
-        ax.plot(df_spike_feature["peak_time"], df_spike_feature["peak_voltage"], "ro",
-                label="peak")
-        ax.plot([time_interpolated[ind] for ind in df_spike_feature["min_AHP_indices"].astype(int)],
-                df_spike_feature["min_AHP_values"], "ko",
-                label="min_AHP")
+        ax.plot(df_spike_feature["peak_time"], df_spike_feature["peak_voltage"],
+                "ro", label="peak")
 
-        # ["min_between_peaks_indices"]
-        ax.plot([time_interpolated[ind] for ind in df_spike_feature["min_between_peaks_indices"].astype(int)],
+        # Plot min_AHP
+        min_ahp_indices = df_spike_feature["min_AHP_indices"].astype(int)
+        ax.plot([time_interpolated[ind] for ind in min_ahp_indices],
+                df_spike_feature["min_AHP_values"], "ko", label="min_AHP")
+
+        # Plot min_between_peaks
+        min_between_indices = df_spike_feature["min_between_peaks_indices"].astype(int)
+        ax.plot([time_interpolated[ind] for ind in min_between_indices],
                 df_spike_feature["min_between_peaks_values"], "bo",
                 label="min_between_peaks")
-
-        # for i in range(df_features["spike_count"][0][0] - 1):
-        #     ax.axhline(df_features["depolarized_base"][0][i], color="k", linestyle="--")
 
     # Plot sag, if "SubThresh" in stim_code
     if "SubThresh" in df_sweep_meta["stimulus_code"].values[0]:
@@ -57,22 +82,34 @@ def plot_sweep_raw(raw_trace, df_sweep_meta, df_sweep_feature, df_spike_feature)
         ax.plot([stim_start, stim_start],
                 [df_sweep_feature["minimum_voltage"],
                  df_sweep_feature["minimum_voltage"] + sag_amplitude],
-                color="deepskyblue", ls="-", label=f"sag_amplitude = {sag_amplitude:.2f}")
+                color="deepskyblue", ls="-",
+                label=f"sag_amplitude = {sag_amplitude:.2f}")
+
         voltage_deflection = df_sweep_feature["voltage_deflection"]
         ax.plot([stim_start, stim_start],
                 [steady_state_voltage_stimend,
-                    steady_state_voltage_stimend + (-voltage_deflection)],
-                color="red", ls="-", label=f"voltage_deflection = {voltage_deflection:.2f}")
+                 steady_state_voltage_stimend + (-voltage_deflection)],
+                color="red", ls="-",
+                label=f"voltage_deflection = {voltage_deflection:.2f}")
 
     # Add stimulus trace
     ax_stimulus.plot(time, stimulus, 'k-', lw=2)
 
+    # Set labels and title
     ax.set_xlabel('Time (ms)')
     ax.set_ylabel('V (mV)')
-    ax.set_title(
-        f'{df_sweep_meta.ephys_roi_id.values[0]} sweep #{df_sweep_meta.sweep_number.values[0]}, {df_sweep_meta.stimulus_code.values[0]}')
-    ax.set_xlim(stim_start - max(3, (stim_end - stim_start) * 0.2),
-                stim_end + max(100, (stim_end - stim_start) * 0.4))
+    title = (
+        f'{df_sweep_meta.ephys_roi_id.values[0]} sweep '
+        f'#{df_sweep_meta.sweep_number.values[0]}, '
+        f'{df_sweep_meta.stimulus_code.values[0]}'
+    )
+    ax.set_title(title)
+
+    # Set x-axis limits
+    xlim_start = stim_start - max(3, (stim_end - stim_start) * 0.2)
+    xlim_end = stim_end + max(150, (stim_end - stim_start) * 0.4)
+    ax.set_xlim(xlim_start, xlim_end)
+
     ax.legend(loc="best", fontsize=12)
     ax.label_outer()
     ax.grid(True)
@@ -81,8 +118,25 @@ def plot_sweep_raw(raw_trace, df_sweep_meta, df_sweep_feature, df_spike_feature)
     return fig
 
 
-def plot_overlaid_spikes(spike_this, df_spike_feature, efel_settings, width_scale: float = 3, beta: float = 2):
-
+def plot_overlaid_spikes(
+    spike_this: pd.DataFrame,
+    df_spike_feature: pd.DataFrame,
+    efel_settings: Dict[str, Any],
+    width_scale: float = 3,
+    beta: float = 3
+) -> plt.Figure:
+    """Plot overlaid spike waveforms with features.
+    
+    Args:
+        spike_this: DataFrame containing spike waveforms
+        df_spike_feature: DataFrame containing spike features
+        efel_settings: Dictionary containing eFEL settings
+        width_scale: Scale factor for line widths
+        beta: Decay factor for line opacity
+        
+    Returns:
+        Matplotlib figure object
+    """
     n_spikes = len(spike_this)
     t = spike_this.columns
     peak_time_idx_in_t = np.argmin(np.abs(t - 0))
@@ -94,10 +148,9 @@ def plot_overlaid_spikes(spike_this, df_spike_feature, efel_settings, width_scal
     ax_v, ax_phase = axs
 
     # Plot the features for the first spike
-    for i in range(n_spikes):
+    for i in reversed(range(n_spikes)):
         v = spike_this.query("spike_idx == @i").values[0]
         peak_time = df_spike_feature["peak_time"].loc[i]
-        # This only works for non-interpolated case. (TIME_STEP = interp_step)
         peak_time_idx_in_raw = int(peak_time / TIME_STEP)
         dvdt = np.gradient(v, t)
 
@@ -105,8 +158,6 @@ def plot_overlaid_spikes(spike_this, df_spike_feature, efel_settings, width_scal
         if i > 0:
             # Only overlay the traces for following spikes with a lower opacity
             ax_v.plot(t, v, 'k', lw=widths[i], alpha=alphas[i])
-
-            # Plot the phase plot
             ax_phase.plot(v, dvdt, 'k', lw=widths[i], alpha=alphas[i])
             continue
 
@@ -120,30 +171,24 @@ def plot_overlaid_spikes(spike_this, df_spike_feature, efel_settings, width_scal
                   label="peak", ms=10)
 
         # min_AHP
-        ax_v.plot(t[df_spike_feature["min_AHP_indices"].loc[0].astype(int) 
-                    - peak_time_idx_in_raw 
+        ax_v.plot(t[df_spike_feature["min_AHP_indices"].loc[0].astype(int)
+                    - peak_time_idx_in_raw
                     + peak_time_idx_in_t],
                   df_spike_feature["min_AHP_values"].loc[0], "ko",
                   label="min_AHP", ms=10)
 
-        # AP_end
-    #     ax_v.axvline(time_interpolated[df_features["AP_end_indices"][0][i]] - peak_time,
-    #            color="b", label="AP_end")
-
         # AP_begin
-        # Spike start is defined as where the first derivative of the voltage trace is higher than 10 V/s, for at least 5 points
         t_begin = df_spike_feature["AP_begin_time"].loc[i] - peak_time
         v_begin = df_spike_feature["AP_begin_voltage"].loc[i]
         ax_v.plot(t_begin, v_begin, "go", label="AP_begin", ms=10)
 
         # AP_begin_width
-        # https://efel.readthedocs.io/en/latest/eFeatures.html#ap-begin-width-ap1-begin-width-ap2-begin-width
         AP_begin_width = df_spike_feature["AP_begin_width"].loc[i]
         ax_v.plot([t_begin, t_begin + AP_begin_width],
-                  [v_begin, v_begin], "g-", label=f"AP_begin_width = {AP_begin_width:.2f}")
+                  [v_begin, v_begin], "g-",
+                  label=f"AP_begin_width = {AP_begin_width:.2f}")
 
         # AP_width
-        # https://efel.readthedocs.io/en/latest/eFeatures.html#ap-width
         threshold = efel_settings["Threshold"]
         threshold_time_idx = np.where(v >= threshold)[0][0]
         threshold_time = t[threshold_time_idx]
@@ -151,23 +196,19 @@ def plot_overlaid_spikes(spike_this, df_spike_feature, efel_settings, width_scal
         ax_v.plot(threshold_time, threshold, "ko", fillstyle="none",
                   ms=10, label=f"Threshold $\equiv$ {threshold} mV")
         ax_v.plot([threshold_time, threshold_time + AP_width],
-                  [threshold, threshold], "k-", label=f"AP_width = {AP_width:.2f}")
-
-        # AP_width_between_threshold
-        # https://efel.readthedocs.io/en/latest/eFeatures.html#ap-width-between-threshold
-        # Should be the same as AP_width for most cases
+                  [threshold, threshold], "k-",
+                  label=f"AP_width = {AP_width:.2f}")
 
         # AP_duration_half_width
-        # https://efel.readthedocs.io/en/latest/eFeatures.html#ap-duration-half-width
         half_rise_time = t[df_spike_feature["AP_rise_indices"].loc[0].astype(
             int) - peak_time_idx_in_raw + peak_time_idx_in_t]
-    #     half_fall_time = time_interpolated[df_features["AP_fall_indices"][0][i]] - peak_time
         half_voltage = (df_spike_feature["AP_begin_voltage"].loc[i] +
                         df_spike_feature["peak_voltage"].loc[i]) / 2
         AP_duration_half_width = df_spike_feature["AP_duration_half_width"].loc[i]
         ax_v.plot(half_rise_time, half_voltage, "mo", ms=10)
         ax_v.plot([half_rise_time, half_rise_time + AP_duration_half_width],
-                  [half_voltage, half_voltage], "m-", label=f"AP_duration_half_width = {AP_duration_half_width:.2f}")
+                  [half_voltage, half_voltage], "m-",
+                  label=f"AP_duration_half_width = {AP_duration_half_width:.2f}")
 
         # Phase plot: phaseslope
         begin_ind = np.where(t >= t_begin)[0][0]
@@ -175,33 +216,37 @@ def plot_overlaid_spikes(spike_this, df_spike_feature, efel_settings, width_scal
         ax_phase.axhline(efel_settings["DerivativeThreshold"],
                          color="g", linestyle=":", label="Derivative threshold")
 
-        # Phase plot: AP_phaseslope
-        xx = np.linspace(v[begin_ind], v[begin_ind] + 10, 100)
-        yy = dvdt[begin_ind] + (xx - v[begin_ind]) * df_spike_feature["AP_phaseslope"].loc[i]
-        ax_phase.plot(xx, yy, "g--", label=f"AP_phaseslope")
-
         # Phase plot: AP_peak_upstroke
-        ax_phase.axhline(df_spike_feature["AP_peak_upstroke"].loc[i],
+        peak_upstroke = df_spike_feature["AP_peak_upstroke"].loc[i]
+        ax_phase.axhline(peak_upstroke,
                          color="c", linestyle="--", label="AP_peak_upstroke")
 
         # Phase plot: AP_peak_downstroke
         ax_phase.axhline(df_spike_feature["AP_peak_downstroke"].loc[i],
                          color="darkblue", linestyle="--", label="AP_peak_downstroke")
+        
+        # Phase plot: AP_phaseslope
+        phaselope = df_spike_feature["AP_phaseslope"].loc[i]
+        dxx = min(-v[begin_ind], peak_upstroke / phaselope)
+        xx = np.linspace(v[begin_ind], v[begin_ind] + dxx, 100)
+        yy = dvdt[begin_ind] + (xx - v[begin_ind]) * phaselope
+        ax_phase.plot(xx, yy, "g--", label="AP_phaseslope")
 
-    # For 1D array or single subplot
+
+    # Set labels and title
     ax_v.set_xlim(-2, 6)
     ax_v.set_xlabel('Time (ms)')
     ax_v.set_ylabel('V (mV)')
     ax_v.set_title(f'Overlaid spikes (n = {n_spikes})')
     ax_v.legend(loc="best", fontsize=12, title="1st spike features",
-                title_fontsize=13)  # Outside the plot
+                title_fontsize=13)
     ax_v.grid(True)
 
     ax_phase.set_xlabel('Voltage (mV)')
     ax_phase.set_ylabel('dv/dt (mV/ms)')
     ax_phase.set_title('Phase Plots')
     ax_phase.legend(loc="best", fontsize=12, title="1st spike features",
-                    title_fontsize=13)  # Outside the plot
+                    title_fontsize=13)
     ax_phase.grid(True)
 
     fig.tight_layout()
@@ -210,10 +255,19 @@ def plot_overlaid_spikes(spike_this, df_spike_feature, efel_settings, width_scal
     return fig
 
 
-def plot_sweep_summary(raw_traces, features_dict, save_dir):
-
+def plot_sweep_summary(
+    raw_traces: List[Dict[str, Any]],
+    features_dict: Dict[str, Any],
+    save_dir: str
+) -> None:
+    """Generate and save sweep summary plots.
+    
+    Args:
+        raw_traces: List of raw trace dictionaries
+        features_dict: Dictionary containing features
+        save_dir: Directory to save plots
+    """
     ephys_roi_id = features_dict["df_sweeps"]["ephys_roi_id"][0]
-
     os.makedirs(f"{save_dir}/{ephys_roi_id}", exist_ok=True)
 
     for raw_trace in raw_traces:
@@ -225,13 +279,29 @@ def plot_sweep_summary(raw_traces, features_dict, save_dir):
             sweep_number] if has_spikes else None
         df_sweep_meta = features_dict["df_sweeps"].query("sweep_number == @sweep_number")
 
-        fig_sweep = plot_sweep_raw(raw_trace, df_sweep_meta, df_sweep_feature, df_spike_feature)
+        # Plot raw sweep
+        fig_sweep = plot_sweep_raw(raw_trace, df_sweep_meta, df_sweep_feature,
+                                   df_spike_feature)
         fig_sweep.savefig(
-            f"{save_dir}/{ephys_roi_id}/{ephys_roi_id}_sweep_{sweep_number}.png", dpi=400)
+            f"{save_dir}/{ephys_roi_id}/{ephys_roi_id}_sweep_{sweep_number}.png",
+            dpi=400
+        )
+        plt.close(fig_sweep)
 
+        # Plot spikes if present
         if has_spikes:
-            spike_this = features_dict["df_spike_waveforms"].query("sweep_number == @sweep_number")
+            spike_this = features_dict["df_spike_waveforms"].query(
+                "sweep_number == @sweep_number"
+            )
             fig_spikes = plot_overlaid_spikes(
-                spike_this, df_spike_feature, features_dict["efel_settings"][0], width_scale=3, beta=2)
+                spike_this,
+                df_spike_feature,
+                features_dict["efel_settings"].iloc[0].to_dict(),
+                width_scale=3,
+                beta=3
+            )
             fig_spikes.savefig(
-                f"{save_dir}/{ephys_roi_id}/{ephys_roi_id}_spikes_{sweep_number}_spikes.png", dpi=400)
+                f"{save_dir}/{ephys_roi_id}/{ephys_roi_id}_spikes_{sweep_number}_spikes.png",
+                dpi=400
+            )
+            plt.close(fig_spikes)
--- src/LCNE_patchseq_analysis/efel/single_cell.py
+++ src/LCNE_patchseq_analysis/efel/single_cell.py
@@ -2,6 +2,8 @@
 
 import logging
 import os
+from typing import Dict, List, Tuple, Optional, Any
+
 import efel
 import pandas as pd
 import numpy as np
@@ -12,10 +14,19 @@ from LCNE_patchseq_analysis.efel.io import save_dict_to_hdf5
 from LCNE_patchseq_analysis.efel.plot import plot_sweep_summary
 
 logger = logging.getLogger(__name__)
-    
 
-def pack_traces_for_efel(raw):
-    """Package traces for eFEL."""
+
+def pack_traces_for_efel(raw: PatchSeqNWB) -> Tuple[List[Dict[str, Any]], np.ndarray]:
+    """Package traces for eFEL.
+    
+    Args:
+        raw: PatchSeqNWB object containing the raw data
+        
+    Returns:
+        Tuple containing:
+        - List of trace dictionaries for eFEL
+        - Array of valid sweep numbers
+    """
     # Only valid sweeps (passed not NA, and exclude CHIRP sweeps)
     df_sweeps = raw.df_sweeps
     valid_sweep_numbers = df_sweeps.loc[
@@ -33,7 +44,6 @@ def pack_traces_for_efel(raw):
         stim_start = meta_this["stimulus_start_time"].values[0]
         stim_end = stim_start + meta_this["stimulus_duration"].values[0]
 
-        # Package raw trace)
         traces.append({
             "T": time,
             "V": trace,
@@ -46,26 +56,19 @@ def pack_traces_for_efel(raw):
     return traces, valid_sweep_numbers
 
 
-def reformat_features(df_features, if_save_interpolated: bool = False):
+def reformat_features(
+    df_features: pd.DataFrame,
+    if_save_interpolated: bool = False
+) -> Dict[str, Any]:
     """Reformat features extracted from eFEL.
     
-    This function processes the raw features dictionary and creates two DataFrames:
-    1. A per-sweep DataFrame with scalar values (single values per sweep)
-    2. A per-spike DataFrame for features that have multiple values per sweep
-    
-    For multi-spike features, the first spike's value is also stored in the per-sweep
-    DataFrame with a 'first_spike_' prefix.
-    
     Args:
-        features (dict): Dictionary of features extracted by eFEL
-        if_save_interpolated (bool): Whether to save the interpolated data
-            By default, interp_step is set to 0.02 ms, which is the same as the sampling rate.
-            So there is no need to save the interpolated data.
+        df_features: DataFrame of features extracted by eFEL
+        if_save_interpolated: Whether to save the interpolated data
         
     Returns:
-        dict: Dictionary containing reformatted DataFrames and interpolated data
+        Dictionary containing reformatted DataFrames and interpolated data
     """
-
     # Create a new DataFrame for per-spike features
     list_features_per_spike = []
 
@@ -73,19 +76,20 @@ def reformat_features(df_features, if_save_interpolated: bool = False):
     dict_features_per_sweep = {}
 
     # Pop time and voltage columns and save to interpolated data if requested
+    interpolated_data = {}
     if if_save_interpolated:
-        interpolated_time = df_features["time"]
-        interpolated_voltage = df_features["voltage"]
+        interpolated_data["interpolated_time"] = df_features["time"]
+        interpolated_data["interpolated_voltage"] = df_features["voltage"]
     df_features.drop(columns=["time", "voltage"], inplace=True)
 
     # Process each column in the original DataFrame
     for col in df_features.columns:
         lengths = df_features[col].map(lambda x: 0 if x is None else len(x))
-        
+
         # If all values are None, skip this column
         if max(lengths) == 0:
             continue
-        
+
         # Check if it's a scalar or array feature
         if max(lengths) == 1:
             # For single values, extract the scalar
@@ -98,76 +102,102 @@ def reformat_features(df_features, if_save_interpolated: bool = False):
             dict_features_per_sweep[f"first_spike_{col}"] = df_features[col].apply(
                 lambda x: x[0] if x is not None and len(x) > 0 else None
             )
-            
+
             # 2. Expand to per-spike DataFrame
             for sweep_idx, sweep_values in df_features[col].items():
                 if sweep_values is not None and len(sweep_values) > 0:
                     list_features_per_spike.extend([
-                        {"sweep_number": sweep_idx, "spike_idx": i, "feature": col, "value": val}
+                        {
+                            "sweep_number": sweep_idx,
+                            "spike_idx": i,
+                            "feature": col,
+                            "value": val
+                        }
                         for i, val in enumerate(sweep_values)
                     ])
 
     # Pack dataframes
     df_features_per_sweep = pd.DataFrame(dict_features_per_sweep)
     df_features_per_spike = pd.DataFrame(list_features_per_spike).pivot(
-        index=["sweep_number", "spike_idx"], 
-        columns="feature", 
+        index=["sweep_number", "spike_idx"],
+        columns="feature",
         values="value"
     )
-    
-    dict_to_save = {
+
+    result_dict = {
         "df_features_per_sweep": df_features_per_sweep,
         "df_features_per_spike": df_features_per_spike,
     }
-    
+
     if if_save_interpolated:
-        dict_to_save["interpolated_time"] = interpolated_time
-        dict_to_save["interpolated_voltage"] = interpolated_voltage
-    
-    return dict_to_save
+        result_dict.update(interpolated_data)
+
+    return result_dict
 
 
-def extract_spike_waveforms(raw_traces, features_dict, spike_window: tuple = (-5, 10)):
+def extract_spike_waveforms(
+    raw_traces: List[Dict[str, Any]],
+    features_dict: Dict[str, Any],
+    spike_window: Tuple[float, float] = (-5, 10)
+) -> pd.DataFrame:
     """Extract spike waveforms from raw data.
     
     Args:
-        raw_traces: raw traces
+        raw_traces: List of raw trace dictionaries
         features_dict: Dictionary containing features extracted by eFEL
-        spike_window: Tuple of two integers, the start and end of the spike window
+        spike_window: Tuple of two floats, the start and end of the spike window
             in milliseconds relative to the peak time
+            
+    Returns:
+        DataFrame containing spike waveforms
     """
     peak_times = features_dict["df_features_per_spike"].reset_index(
-        ).set_index("sweep_number")["peak_time"]
-    
+    ).set_index("sweep_number")["peak_time"]
+
     # Time can be determined by the sampling rate
     t_aligned = np.arange(spike_window[0], spike_window[1], step=TIME_STEP)
     vs = []
-    
+
     for raw_trace in raw_traces:
         if raw_trace["sweep_number"][0] not in peak_times.index:
-            continue    
+            continue
         peak_times_this_sweep = peak_times.loc[raw_trace["sweep_number"]]
         t = raw_trace["T"]
         v = raw_trace["V"]
-        
+
         for peak_time in peak_times_this_sweep:
-            idx = np.where((t >= peak_time + spike_window[0]) & (t < peak_time + spike_window[1]))[0]
+            idx = np.where(
+                (t >= peak_time + spike_window[0]) &
+                (t < peak_time + spike_window[1])
+            )[0]
             v_this = v[idx]
             vs.append(v_this)
-            
-    df_spike_waveforms = pd.DataFrame(vs, 
-                                      index=features_dict["df_features_per_spike"].index,
-                                      columns=pd.Index(t_aligned, name="ms_to_peak"),
-                                      )
-    return df_spike_waveforms
+
+    return pd.DataFrame(
+        vs,
+        index=features_dict["df_features_per_spike"].index,
+        columns=pd.Index(t_aligned, name="ms_to_peak"),
+    )
 
 
-def extract_features_using_efel(raw, if_save_interpolated):
-    """Extract features using eFEL."""
-
+def extract_features_using_efel(
+    raw: PatchSeqNWB,
+    if_save_interpolated: bool
+) -> Tuple[Dict[str, Any], List[Dict[str, Any]]]:
+    """Extract features using eFEL.
+    
+    Args:
+        raw: PatchSeqNWB object containing the raw data
+        if_save_interpolated: Whether to save interpolated data
+        
+    Returns:
+        Tuple containing:
+        - Dictionary of extracted features
+        - List of raw traces
+    """
     # -- Package all valid sweeps for eFEL --
     raw_traces, valid_sweep_numbers = pack_traces_for_efel(raw)
-      
+
     # Get all features
     logger.debug(f"Getting features for {len(raw_traces)} traces...")
     features = efel.get_feature_values(
@@ -176,12 +206,12 @@ def extract_features_using_efel(raw, if_save_interpolated):
         raise_warnings=False,
     )
     logger.debug("Done!")
-    
+
     # Reformat features
     df_features = pd.DataFrame(features, index=valid_sweep_numbers)
     df_features.index.name = "sweep_number"
     features_dict = reformat_features(df_features, if_save_interpolated)
-    
+
     # -- Extract spike waveforms --
     df_spike_waveforms = extract_spike_waveforms(raw_traces, features_dict)
 
@@ -197,17 +227,27 @@ def extract_features_using_efel(raw, if_save_interpolated):
         .rename(columns=col_to_df_sweeps)
     )
     df_sweeps = df_sweeps.merge(_df_to_df_sweeps, on="sweep_number", how="left")
-                   
+
     # Add metadata to features_dict
     features_dict["df_sweeps"] = df_sweeps
     features_dict["df_spike_waveforms"] = df_spike_waveforms
-    features_dict["efel_settings"] = pd.Series([efel.get_settings().__dict__])
-    
+    features_dict["efel_settings"] = pd.DataFrame([efel.get_settings().__dict__])
+
     return features_dict, raw_traces
 
 
-def process_one_nwb(ephys_roi_id: str, if_save_interpolated: bool = False, save_dir: str = "results"):
-        
+def process_one_nwb(
+    ephys_roi_id: str,
+    if_save_interpolated: bool = False,
+    save_dir: str = "results"
+) -> None:
+    """Process one NWB file.
+    
+    Args:
+        ephys_roi_id: ID of the electrophysiology ROI
+        if_save_interpolated: Whether to save interpolated data
+        save_dir: Directory to save results
+    """
     # --- 1. Get raw data ---
     raw = PatchSeqNWB(ephys_roi_id=ephys_roi_id)
 
@@ -217,26 +257,26 @@ def process_one_nwb(ephys_roi_id: str, if_save_interpolated: bool = False, save_
     # --- 3. Save features_dict to HDF5 using panda's hdf5 store ---
     os.makedirs(f"{save_dir}/features", exist_ok=True)
     save_dict_to_hdf5(features_dict, f"{save_dir}/features/{ephys_roi_id}_efel_features.h5")
-    
+
     # --- 4. Generate sweep plots ---
     # Append stimulus to raw_traces
     for raw_trace in raw_traces:
         raw_trace["stimulus"] = raw.get_stimulus(raw_trace["sweep_number"][0])
     plot_sweep_summary(raw_traces, features_dict, f"{save_dir}/plots")
-    return
 
 
 if __name__ == "__main__":
     import tqdm
-    
+
     logging.basicConfig(level=logging.INFO)
 
     from LCNE_patchseq_analysis.data_util.metadata import load_ephys_metadata
 
     df_meta = load_ephys_metadata()
-    
-    for _ephys_roi_id in tqdm.tqdm(df_meta["ephys_roi_id_tab_master"][:1]):
+
+    for _ephys_roi_id in ["1408897096"]: # tqdm.tqdm(df_meta["ephys_roi_id_tab_master"][:1]):
         logger.info(f"Processing {_ephys_roi_id}...")
-        process_one_nwb(ephys_roi_id=str(int(_ephys_roi_id)), 
-                        if_save_interpolated=False)
- 
+        process_one_nwb(
+            ephys_roi_id=str(int(_ephys_roi_id)),
+            if_save_interpolated=False
+        )
