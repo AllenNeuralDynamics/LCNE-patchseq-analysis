966531badabd24a0aed9bc48e102860b099b623a
Hou, Han <houhan@gmail.com>
2025-04-02 22:30:06 -0700

    minor

--- src/LCNE_patchseq_analysis/data_util/metadata.py
+++ src/LCNE_patchseq_analysis/data_util/metadata.py
@@ -95,7 +95,6 @@ def load_ephys_metadata():
 
     # Change columns with roi_id to str(int())
     for col in ["ephys_roi_id_tab_master", "ephys_roi_id_lims"]:
-        df[col] = df[col].astype('object')
         df.loc[:, col] = df[col].apply(lambda x: str(int(x)) if pd.notna(x) else "")
     return df
 
