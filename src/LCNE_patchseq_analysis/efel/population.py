be1535371161788cf35faae52272d2076867ac94
Hou, Han <houhan@gmail.com>
2025-04-03 21:35:10 -0700

    linting

--- src/LCNE_patchseq_analysis/efel/population.py
+++ src/LCNE_patchseq_analysis/efel/population.py
@@ -9,21 +9,25 @@ from typing import Literal
 import pandas as pd
 
 from LCNE_patchseq_analysis.efel import (
-    EXTRACT_SPIKE_FROMS, EXTRACT_SAG_FROMS,
-    EXTRACT_SPIKE_FEATURES, EXTRACT_SAG_FEATURES
+    EXTRACT_SPIKE_FROMS,
+    EXTRACT_SAG_FROMS,
+    EXTRACT_SPIKE_FEATURES,
+    EXTRACT_SAG_FEATURES,
 )
 from LCNE_patchseq_analysis.efel.io import load_efel_features_from_roi
 
 logger = logging.getLogger(__name__)
 
 
-
-def df_sweep_selector(df: pd.DataFrame,
-                   stim_type: Literal["subthreshold", "short_square_rheo",
-                                      "long_square_rheo", "long_square_supra"],
-                   aggregate_method: Literal["min", "aver"] | int) -> pd.DataFrame:
+def df_sweep_selector(  # noqa: C901
+    df: pd.DataFrame,
+    stim_type: Literal[
+        "subthreshold", "short_square_rheo", "long_square_rheo", "long_square_supra"
+    ],
+    aggregate_method: Literal["min", "aver"] | int,
+) -> pd.DataFrame:
     """Select sweeps based on stimulus type and aggregation method."""
-    
+
     def _get_min_or_aver(df_this, aggregate_method):
         if aggregate_method == "aver":
             return df_this
@@ -33,13 +37,12 @@ def df_sweep_selector(df: pd.DataFrame,
             return df_this.loc[[min_idx]]
         else:
             raise ValueError("aggregate_method must be 'aver' or 'min'")
-    
+
     if stim_type == "subthreshold":
         if aggregate_method == "aver":
             # All SubThreshold sweeps
             return df.query(
-                "stimulus_code.str.contains('SubThresh')"
-                "and stimulus_name == 'Long Square'"
+                "stimulus_code.str.contains('SubThresh')" "and stimulus_name == 'Long Square'"
             )
         elif isinstance(aggregate_method, int):
             return df.query(
@@ -49,8 +52,10 @@ def df_sweep_selector(df: pd.DataFrame,
                 " and abs(abs(stimulus_amplitude) - abs(@aggregate_method)) < 1"
             )
         else:
-            raise ValueError("aggregate_method must be 'aver' or an integer (the abs(amplitude)"
-                             "of the stimulus) for subthreshold sweeps")
+            raise ValueError(
+                "aggregate_method must be 'aver' or an integer (the abs(amplitude)"
+                "of the stimulus) for subthreshold sweeps"
+            )
     elif stim_type == "short_square_rheo":
         df_this = df.query(
             "stimulus_code.str.contains('Rheo')"
@@ -97,15 +102,20 @@ def extract_cell_level_stats_one(ephys_roi_id: str):
         ]:
             for key, value in feature_type.items():
                 df_sweep = df_sweep_selector(
-                    df_features_per_sweep, stim_type=value[0], aggregate_method=value[1])
+                    df_features_per_sweep, stim_type=value[0], aggregate_method=value[1]
+                )
                 if df_sweep is not None:
                     # Calculate mean over rows for each feature
                     mean_values = df_sweep[features_to_extract].mean()
                     # Create a dictionary with feature names and their mean values
-                    feature_dict = {f"{feature} @ {key}": value for feature, value in mean_values.items()}
+                    feature_dict = {
+                        f"{feature} @ {key}": value for feature, value in mean_values.items()
+                    }
                     cell_stats_dict.update(feature_dict)
 
-        cell_stats = pd.DataFrame(cell_stats_dict, index=pd.Index([ephys_roi_id], name="ephys_roi_id"))
+        cell_stats = pd.DataFrame(
+            cell_stats_dict, index=pd.Index([ephys_roi_id], name="ephys_roi_id")
+        )
 
         logger.info(f"Successfully extracted cell-level stats for {ephys_roi_id}!")
         return "Success", cell_stats
@@ -115,7 +125,6 @@ def extract_cell_level_stats_one(ephys_roi_id: str):
         error_message = f"Error processing {ephys_roi_id}: {str(e)}\n{traceback.format_exc()}"
         logger.error(error_message)
         return None
-
 
 
 if __name__ == "__main__":
